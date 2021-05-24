824c3e43df302b4a96b7f4b93ab3db5836704848
Barbara Hartmann <barbara.hartmann@gmail.com>
2021-05-24 14:54:17 +0200

    added some fun functionality in lib.days_left()

--- babs_package/lib.py
+++ babs_package/lib.py
@@ -1,2 +1,9 @@
+from datetime import datetime
+
 def try_me():
     return "Barbara says hello!"
+
+def days_left():
+    today = datetime.now()
+    the_end = datetime(year=2021, month=6, day=11)
+    return(f'There are only {(the_end - today).days} days left until the last day of this amazing bootcamp!')
--- tests/test_lib.py
+++ tests/test_lib.py
@@ -1,4 +1,5 @@
-from babs_package.lib import try_me
+from babs_package.lib import days_left, try_me
 
 def test_try_me():
     assert try_me() != ""
+    assert type(days_left()) == str
